c03dcd5c8bbd73980bada7e8dc4517839ff12195
Iramar Oliveira <iramaroliveira1@hotmail.com>
2021-02-26 23:42:34 -0300

    Api de cadastro, validação dos inputs, traduzindo mensagens de erros

---
+++ app/Http/Controllers/cliente/ClienteController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\cliente;
+
+use App\Http\Controllers\Controller;
+use App\Models\Cliente;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class ClienteController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+
+            $updateField = [
+                'nome' => $request->name,
+                'email' => $request->email,
+                'senha' => $request->password
+            ];
+
+            $validate = Validator::make($updateField, [
+                'nome' => 'required',
+                'email' => 'required|email|unique:clientes',
+                'senha' => 'required'
+            ]);
+
+            if ($validate->fails()) {
+                return response()->json([
+                    'message' => $validate->errors()->messages(),
+                    'error' => true
+                ], 400);
+            }
+
+            $cryptography = Hash::make($request->password);
+
+            $save = Cliente::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $cryptography
+            ]);
+
+            return response()->json([
+                'data' => $save,
+                'error' => false
+            ], 201);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => $th,
+                'error' => true
+            ], 400);
+        }
+    }
+}
